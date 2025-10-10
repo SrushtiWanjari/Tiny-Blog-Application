163873a9f8418a62c213f5f6d93590d901d6a4c8
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-10-10 19:02:32 +0530

    User Schema added

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,15 @@
-import {model, Schema} from "mongoose";
+import { model, Schema } from "mongoose";
 
-const userSchema = new Schema({});
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = model("User", userSchema);
 
