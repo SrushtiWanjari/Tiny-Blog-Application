dd0ccc6d626e100a040f8aeb694e3a28c3158075
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-10-13 22:30:57 +0530

    basic signup controller added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,19 @@
-const postSignup = (req, res) => {}
+import User from "./../models/User";
 
-const postLogin = (req, res) => {}
+const postSignup = async (req, res) => {
+  const { name, email, password } = req.body;
+  
+  const newUser = new User({ name, email, password });
 
-export {postSignup, postLogin};
+  const savedUser = await newUser.save();
+
+  res.json({
+    success: true,
+    message: "User registered successfully",
+    user: savedUser,
+  });
+};
+
+const postLogin = (req, res) => {};
+
+export { postSignup, postLogin };
