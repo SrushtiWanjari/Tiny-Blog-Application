4d996e4096d5ba5ea4ff92323bb8990a6e6fa27a
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-10-14 19:16:47 +0530

    routing completed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,22 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import AllBlogs from "./views/AllBlogs.jsx";
+import NewBlog from "./views/NewBlog.jsx";
+import EditBlog from "./views/EditBlog.jsx";
+import ReadBlog from "./views/ReadBlog.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AllBlogs />} />
+      <Route path="/new" element={<NewBlog />} />
+      <Route path="/edit/:id" element={<EditBlog />} />
+      <Route path="/blog/:slug" element={<ReadBlog />} />
+      <Route
+        path="*"
+        element={<h1 className="text-center mt-5">404 Not Found</h1>}
+      />
+    </Routes>
+  </BrowserRouter>
+);
