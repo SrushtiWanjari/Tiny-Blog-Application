0a093938fe0ae98922b38b6522455c2a34df71ca
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-10-14 17:15:39 +0530

    condition added for filling info

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,6 +3,27 @@ import User from "./../models/User.js";
 const postSignup = async (req, res) => {
   const { name, email, password } = req.body;
 
+  if(!name){
+    return res.status(400).json({
+      success: false,
+      message: "Name is required",
+    });
+  }
+
+  if(!email){
+    return res.status(400).json({
+      success: false,
+      message: "Email is required",
+    });
+  }
+
+  if(!password){
+    return res.status(400).json({
+      success: false,
+      message: "Password is required",
+    });
+  }
+
   const existingUser = await User.findOne({email});
   if(existingUser){
     return res.status(400).json({
