2c714f2ab1304c6086ba5d5c09acb56bd683ece5
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-10-14 21:16:04 +0530

    in login page basic code and css added

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import {Link} from "react-router";
 
 function Login() {
   return (
     <div>
-      
+      <h1 className="text-center text-4xl my-4">Login </h1>
+
+      <div
+        className="max-w-[400px] mx-auto border-1
+      border-gray-500 py-10 px-14 rounded-md"
+      >
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-2 rounded w-full mb-4"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-2 rounded w-full mb-4"
+        />
+        <button className="bg-gray-700 text-white px-4 py-2 rounded-md">
+          Login
+        </button>
+
+        <p className="mt-6">
+          Don't have an account?&nbsp;
+          <Link to="/signup" className="text-blue-500 underline">Signup</Link>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- client/src/views/Signup.jsx
+++ client/src/views/Signup.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router";
 
 function Signup() {
   return (
     <div>
-      <h1 className='text-center text-4xl my-4'>Signup</h1>
+      <h1 className="text-center text-4xl my-4">Signup</h1>
 
-      <div className='max-w-[400px] mx-auto border-1
-      border-gray-500 py-10 px-14 rounded-md'>
-        <input type='name' placeholder='Name' className='border p-2 rounded w-full mb-4'/>
-        <input type='email' placeholder='Email' className='border p-2 rounded w-full mb-4'/>
-        <input type='password' placeholder='Password' className='border p-2 rounded w-full mb-4'/>
-        <button className='bg-gray-700 text-white px-4 py-2 rounded-md'>Signup</button>
+      <div
+        className="max-w-[400px] mx-auto border-1
+      border-gray-500 py-10 px-14 rounded-md"
+      >
+        <input
+          type="name"
+          placeholder="Name"
+          className="border p-2 rounded w-full mb-4"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-2 rounded w-full mb-4"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-2 rounded w-full mb-4"
+        />
+        <button className="bg-gray-700 text-white px-4 py-2 rounded-md">
+          Signup
+        </button>
+
+         <p className="mt-6">
+          Already have an account?&nbsp;
+          <Link to="/login" className="text-blue-500 underline">Login</Link>
+        </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
