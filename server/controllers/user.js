6483142c5b6f6fb1ce3673a230a28f3bf953558e
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-10-14 17:44:50 +0530

    code added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,36 +3,38 @@ import User from "./../models/User.js";
 const postSignup = async (req, res) => {
   const { name, email, password } = req.body;
 
-  const emailValidationRegex = "^[^@]+@[^@]+\\.[^@]+$";
-  const nameValidationRegex = "^[a-zA-Z]+$";
-  const passwordValidationRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+  const emailValidationRegex = /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/;
+  const nameValidationRegex = /^[a-zA-Z]+$/;
+  const passwordValidationRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-  if(!name){
+  if (nameValidationRegex.test(name) === false) {
     return res.status(400).json({
       success: false,
-      message: "Name is required",
+      message: "Name is contain only alphabets and spaces",
     });
   }
 
-  if(!email){
+  if (emailValidationRegex.test(email) === false) {
     return res.status(400).json({
       success: false,
-      message: "Email is required",
+      message: "Email is not valid",
     });
   }
 
-  if(!password){
+  if (passwordValidationRegex.test(password) === false) {
     return res.status(400).json({
       success: false,
-      message: "Password is required",
+      message:
+        "Password must be at least 8 characters long and at least one uppercase letter, one lowercase letter, one number, and one special character",
     });
   }
 
-  const existingUser = await User.findOne({email});
-  if(existingUser){
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
     return res.status(400).json({
-        success: false,
-        message: `User with ${email} already exists`,
+      success: false,
+      message: `User with ${email} already exists`,
     });
   }
 
@@ -49,4 +51,4 @@ const postSignup = async (req, res) => {
 
 const postLogin = (req, res) => {};
 
-export { postSignup, postLogin }
+export { postSignup, postLogin };
