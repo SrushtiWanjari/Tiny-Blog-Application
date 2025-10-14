22c5e6f3d413292478b9d79d9e4ff7b3c985ef92
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-10-14 17:09:12 +0530

    changed added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,8 +1,16 @@
-import User from "./../models/User";
+import User from "./../models/User.js";
 
 const postSignup = async (req, res) => {
   const { name, email, password } = req.body;
-  
+
+  const existingUser = await User.findOne({email});
+  if(existingUser){
+    return res.status(400).json({
+        success: false,
+        message: `User with ${email} already exists`,
+    });
+  }
+
   const newUser = new User({ name, email, password });
 
   const savedUser = await newUser.save();
@@ -16,4 +24,4 @@ const postSignup = async (req, res) => {
 
 const postLogin = (req, res) => {};
 
-export { postSignup, postLogin };
+export { postSignup, postLogin }
