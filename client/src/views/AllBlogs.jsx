8e48eda9b0b0c91f2eefd5b70e6a46b296590fb7
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-10-14 21:07:51 +0530

    signup page basic code and css added

--- client/src/views/AllBlogs.jsx
+++ client/src/views/AllBlogs.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function AllBlogs() {
   return (
     <div>
-      
+      <h1>AllBlogs</h1>
     </div>
   )
 }
--- client/src/views/Signup.jsx
+++ client/src/views/Signup.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 function Signup() {
   return (
     <div>
-      
+      <h1 className='text-center text-4xl my-4'>Signup</h1>
+
+      <div className='max-w-[400px] mx-auto border-1
+      border-gray-500 py-10 px-14 rounded-md'>
+        <input type='name' placeholder='Name' className='border p-2 rounded w-full mb-4'/>
+        <input type='email' placeholder='Email' className='border p-2 rounded w-full mb-4'/>
+        <input type='password' placeholder='Password' className='border p-2 rounded w-full mb-4'/>
+        <button className='bg-gray-700 text-white px-4 py-2 rounded-md'>Signup</button>
+      </div>
     </div>
   )
 }
