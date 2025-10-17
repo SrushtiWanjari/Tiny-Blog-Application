d74852f1b41c4ddaa7e8867c8c9e8c8fc7ed34e8
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-10-17 19:04:44 +0530

    user controller code added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,24 +1,26 @@
+import jwt from "jsonwebtoken";
+import md5 from "md5";
 import User from "./../models/User.js";
 
 const postSignup = async (req, res) => {
   const { name, email, password } = req.body;
 
-  if(!name || !email || !password){
+  if (!name || !email || !password) {
     return res.status(400).json({
       success: false,
-      message: "name, email, passsword are requires",
-    })
+      message: "name, email and password are required",
+    });
   }
 
   const emailValidationRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const nameValidationRegex = /^[a-zA-Z]+$/;
+  const nameValidationRegex = /^[a-zA-Z ]+$/;
   const passwordValidationRegex =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
   if (nameValidationRegex.test(name) === false) {
     return res.status(400).json({
       success: false,
-      message: "Name is contain only alphabets and spaces",
+      message: "Name should contain only alphabets and spaces",
     });
   }
 
@@ -33,7 +35,7 @@ const postSignup = async (req, res) => {
     return res.status(400).json({
       success: false,
       message:
-        "Password must be at least 8 characters long and at least one uppercase letter, one lowercase letter, one number, and one special character",
+        "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character",
     });
   }
 
@@ -41,11 +43,11 @@ const postSignup = async (req, res) => {
   if (existingUser) {
     return res.status(400).json({
       success: false,
-      message: `User with ${email} already exists`,
+      message: `User with email ${email} already exists`,
     });
   }
 
-  const newUser = new User({ name, email, password });
+  const newUser = new User({ name, email, password: md5(password) });
 
   const savedUser = await newUser.save();
 
@@ -57,28 +59,43 @@ const postSignup = async (req, res) => {
 };
 
 const postLogin = async (req, res) => {
-   const {email, password} = req.body;
+  const { email, password } = req.body;
 
-   if (!email || !password){
+  if (!email || !password) {
     return res.status(400).json({
       success: false,
       message: "email and password are required",
     });
-   }
+  }
 
-   const existingUser = await User.findOne({email, password});
-   if(existingUser){
+  const existingUser = await User.findOne({
+    email,
+    password: md5(password),
+  }).select("_id name email");
+
+  if (existingUser) {
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+        email: existingUser.email,
+        name: existingUser.name,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
     return res.json({
       success: true,
       message: "User logged in successfully",
       user: existingUser,
-    })
-   } else{
-    return res.status(400).json({
+      token,
+    });
+  } else {
+    return res.status(401).json({
       success: false,
       message: "Invalid email or password",
-    })
-   }
+    });
+  }
 };
 
-export { postSignup, postLogin };
+export { postLogin, postSignup };
