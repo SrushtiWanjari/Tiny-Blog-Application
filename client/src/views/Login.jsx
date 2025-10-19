3e3ca3ef90f1a32f41bd1c9ef294dc72c30114fe
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-10-19 20:14:36 +0530

    login code added

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { Link } from "react-router";
 import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router";
 
 function Login() {
   const [user, setUser] = useState({
@@ -9,18 +9,22 @@ function Login() {
   });
 
   const loginUser = async () => {
-    const response = await axios.post("http://localhost:8080/login", user);
-    console.log(response.data)
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/login`,
+      user
+    );
+    if (response?.data?.success) {
+      localStorage.setItem("loggedInUser", JSON.stringify(response.data.user));
+      localStorage.setItem("token", response.data.token);
+      window.location.href = "/";
+    }
   };
 
   return (
     <div>
-      <h1 className="text-center text-4xl my-4">Login </h1>
+      <h1 className="text-center text-4xl my-4">Login</h1>
 
-      <div
-        className="max-w-[400px] mx-auto border-1
-      border-gray-500 py-10 px-14 rounded-md"
-      >
+      <div className="max-w-[400px] mx-auto border-1 border-gray-500 py-10 px-14 rounded-md">
         <input
           type="email"
           placeholder="Email"
@@ -36,15 +40,15 @@ function Login() {
           onChange={(e) => setUser({ ...user, password: e.target.value })}
         />
         <button
-          className="bg-gray-700 text-white px-4 py-2 rounded-md"
-          type="button"
+          className="bg-gray-700 text-white px-6 py-2 rounded-md"
           onClick={loginUser}
+          type="button"
         >
           Login
         </button>
 
         <p className="mt-6">
-          Don't have an account?&nbsp;
+          Don't have an account?{" "}
           <Link to="/signup" className="text-blue-500 underline">
             Signup
           </Link>
